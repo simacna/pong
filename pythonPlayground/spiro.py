f4157607a220ef87e775e52d4ba45aa0da357574
sina sima <simacna@gmail.com>
2017-07-21 16:55:02 -0400

    python

--- pythonPlayground/spiro.py
+++ pythonPlayground/spiro.py
@@ -13,3 +13,43 @@ def drawCircleTurtle(x, y, r):
 drawCircleTurtle(100, 100, 500)
 turtle.mainloop()
 
+class MyClass:
+  i = 123
+
+  def f(self):
+    return "hi"
+yo = MyClass()
+# print(yo.f()) -- returns error
+
+class myClass:
+  def __init__(self):
+    self.data = [1]
+
+class Complex:
+  def __init__(self, realpart, imagpart):
+    self.r = realpart
+    self.i = imagpart
+
+x = Complex
+x.counter = 1
+while x.counter < 10:
+  x.counter = x.counter * 2
+
+# print(x.counter)
+
+del x.counter
+
+aa = yo.f
+# print(aa())  -- works!
+
+class Dog:
+  def __init__(self, name):
+    self.name = name
+    self.tricks = []
+
+  def add_trick(self, trick):
+    self.tricks.append(trick)
+
+d = Dog('Fido')
+d.add_trick('dance')
+print(d.tricks)
