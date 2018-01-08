2f54ca69a7fa949ba49acec46fcd780220b3a98f
sina sima <simacna@gmail.com>
2018-01-08 16:38:42 -0500

    monday

---
+++ interactivePython/ch1/discussionQuestions.py
@@ -0,0 +1,23 @@
+# Construct a class hierarchy for people on a college campus. Include faculty, staff, 
+# and students. What do they have in common? What distinguishes them from one another?
+
+
+# class People:
+#   def __init__(self, money):
+#     self.money =  money
+
+#   def returnMoney(self, money):
+#     return money*2
+
+
+# test = People(2)
+# print(People.returnMoney(2))
+
+def this_is_a_test(input1):
+  if(input1 == type(int)):
+    print("this is a number")
+  else:
+    print("this isn't an int")
+
+a = this_is_a_test('Friday')
+print(a)
