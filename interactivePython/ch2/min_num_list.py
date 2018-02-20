91ce64ed7be52f7c73eef1596246e09ca53f802e
sina sima <simacna@gmail.com>
2018-02-20 16:59:14 -0500

    tuesday

--- interactivePython/ch2/min_num_list.py
+++ interactivePython/ch2/min_num_list.py
@@ -183,14 +183,16 @@ popzero = timeit.Timer("x.pop(0)",
                 "from __main__ import x")
 popend = timeit.Timer("x.pop()",
                "from __main__ import x")
-print("pop(0)   pop()")
-for i in range(1000000,100000001,1000000):
-    x = list(range(i))
-    pt = popend.timeit(number=1000)
-    x = list(range(i))
-    pz = popzero.timeit(number=1000)
-    print("%15.5f, %15.5f" %(pz,pt))
+# print("pop(0)   pop()")
+# for i in range(1000000,100000001,1000000):
+#     x = list(range(i))
+#     pt = popend.timeit(number=1000)
+#     x = list(range(i))
+#     pz = popzero.timeit(number=1000)
+#     print("%15.5f, %15.5f" %(pz,pt))
 
+alpha = {'a': 1, 'b':2}
+print(alpha['a'])
 
 
 
