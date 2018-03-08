6c371d8da51b1ce61bb167f733d9e7d7762b0032
sina sima <simacna@gmail.com>
2018-03-08 16:02:23 -0500

    wo

--- interactivePython/ch2/discussion.py
+++ interactivePython/ch2/discussion.py
@@ -1,5 +1,12 @@
 #1 give the big-o performance of the following code fragments
 
+O(1)  1 1
+O(log n)  3 7
+O(n)  10  100
+O(n log n)  30  700
+O(n^2)  100 10000
+O(2^n)  1024  2^100
+O(n!)  3628800  100!
 
 
 a = list(range(1,100,10))
@@ -12,3 +19,7 @@ print(b)
 #2 - for i in range(n):  sina answer O(n)
      #k = 2 + 2
 
+#3 - i = n
+  # while i > 0:
+  #    k = 2 + 2
+  #    i = i // 2 --  sina answr O
