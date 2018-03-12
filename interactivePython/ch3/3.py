4e067d7e448673823d5c734e731c7a52acf50b1e
sina sima <simacna@gmail.com>
2018-03-12 19:06:45 -0400

    mondayz

--- interactivePython/ch3/3.py
+++ interactivePython/ch3/3.py
@@ -1,4 +1,48 @@
 # Stack -> first in, last out
 
+
+
+class Stack:
+
+  def __init__(self):
+    self.items = [2]
+
+  def is_empty(self):
+    # if len(self.items == []):
+    #   return True
+    # else:
+    #   return False
+    return self.items == []
+
+  def push(self, item):
+    # print(self.items += [item])
+    return (self.items + [item])
+
+
+  def pop(self,item):
+    return self.items.pop(item)
+
+  def list(self):
+    return self.items
+    print (self.items)
+
+  def test(self):
+    return self.items
+
+  def size(self):
+    return len(self.items)
+
 a = Stack()
-a.push(2)
+print(a.push(3))  #should print out
+# print(list())
+print(a.test())
+
+print(a.size())
+
+# s = Stack()
+# s.push('a')
+# print(s.is_empty())
+# print(s.peek())
+# print(s.is_empty())
+# print(s.pop())
+# print(s.is_empty())
---
+++ interactivePython/ch3/stack.py
@@ -0,0 +1,26 @@
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+     def lists(self):
+         return self.items
+
+a = Stack()
+a.push(3)
+# print(a.size())
+print(a.lists())
