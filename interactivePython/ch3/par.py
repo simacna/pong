13729fd845771eeed546cec02ac35fe8faed4b2d
sina sima <simacna@gmail.com>
2018-03-20 17:17:00 -0400

    tues

--- interactivePython/ch3/par.py
+++ interactivePython/ch3/par.py
@@ -20,6 +20,31 @@ class Stack:
      def lists(self):
          return self.items
 
+def par(pars):
+    balanced = True
+    s = Stack()
+    index = 0
+
+    while index < len(pars) and balanced:
+        item = pars[index]
+        if item == '(':
+            s.push(item)
+        else:
+            s.pop()
+
+        index = index + 1
+
+    if s.isEmpty() and balanced:
+        return True
+    else:
+        return False
+
+print(par('((()))'))
+print(par('(()'))
+
+
+
+
 
 def parChecker(symbolString):
     s = Stack()
@@ -29,7 +54,7 @@ def parChecker(symbolString):
         symbol = symbolString[index]
         if symbol == "(":
             s.push(symbol)
-            print('s.push')
+            # print('s.push')
         else:
             if s.isEmpty():
                 balanced = False
@@ -43,5 +68,5 @@ def parChecker(symbolString):
     else:
         return False
 
-print(parChecker('((()))'))
+# print(parChecker('((()))'))
 # print(parChecker('(()'))
