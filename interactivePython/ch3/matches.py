8266034320e31f47b2834d73cba167baa2a3878b
sina sima <simacna@gmail.com>
2018-03-20 22:51:41 -0400

    matches part II

--- interactivePython/ch3/matches.py
+++ interactivePython/ch3/matches.py
@@ -2,6 +2,12 @@ def matches(inp):
   first_half = len(inp)//2
   first_half_list = inp[0:first_half]
   second_half_list = inp[first_half:]
-  return second_half_list
+  weighted = True
+  index = 0
+  s = Stack()
+  # if (len(inp)%2 = 0):
+  #   while index < len(inp):
+  #     if (inp[index] and )
+  
 
 print(matches([1,1,2,2]))
